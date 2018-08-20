b7172c17af16079445aee429d4959567e4040c0b
Hannah Werman <hanna.w28@gmail.com>
2018-08-20 14:10:33 -0400

    initialized firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const devConfig = {
+  apiKey: "AIzaSyA6WJvsAjC09zDjfrNf5eOQsy1zmj_S_Rc",
+  authDomain: "hostel-social.firebaseapp.com",
+  databaseURL: "https://hostel-social.firebaseio.com",
+  projectId: "hostel-social",
+  storageBucket: "hostel-social.appspot.com",
+  messagingSenderId: "97734006740"
+};
+
+const prodConfig = {
+  // add production database information here
+}
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+if(!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+  auth,
+};
