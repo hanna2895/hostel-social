97709982fd68aabe5f4726560bc73df7ccec6526
Hannah Werman <hanna.w28@gmail.com>
2018-08-23 11:54:29 -0500

    styled landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Landing.css';
+import { Link } from 'react-router-dom';
+import * as routes from '../../constants/routes';
+
 
 const Landing = () => {
   return (
     <div>
-      <h1>Landing Page</h1>
+      <h2>Welcome to</h2>
+      <h1>Hostel Social</h1>
+      <Link to={routes.SIGN_IN} className="sign-up-button">Sign Up</Link>
+
     </div>
   )
 }
