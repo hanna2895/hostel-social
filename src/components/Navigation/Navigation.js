582c350a976304120b5c59dce4a21886392aa9ac
Hannah Werman <hanna.w28@gmail.com>
2018-09-27 14:39:47 +0100

    added appbar and basic menu to dashboard

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AuthUserContext from '../AuthUserContext';
 import SignOutButton from '../SignOut/SignOut';
 import * as routes from '../../constants/routes';
+
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 const Navigation = () =>
   <AuthUserContext.Consumer>
@@ -12,28 +37,81 @@ const Navigation = () =>
     }
   </AuthUserContext.Consumer>
 
-const NavigationAuth = () => {
-  return (
-    <div>
-      <ul>
-        <li><Link to={routes.LANDING}>Landing</Link></li>
-        <li><Link to={routes.HOME}>Home</Link></li>
-        <li><Link to={routes.ACCOUNT}>Account</Link></li>
-        <li><SignOutButton /></li>
-      </ul>
-    </div>
-  )
-}
-
-// const NavigationNonAuth = () => {
+// const NavigationAuth = () => {
 //   return (
 //     <div>
 //       <ul>
 //         <li><Link to={routes.LANDING}>Landing</Link></li>
-//         <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+//         <li><Link to={routes.HOME}>Home</Link></li>
+//         <li><Link to={routes.ACCOUNT}>Account</Link></li>
+//         <li><SignOutButton /></li>
 //       </ul>
 //     </div>
 //   )
 // }
+
+class NavigationAuth extends Component  {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      anchorEl: null,
+    };
+  }
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl)
+    return (
+
+      
+        <AppBar position="static" style={{ marginTop: '2vh' }}>
+          <Toolbar>
+            <div>
+              <IconButton color="inherit" aria-label="Menu">
+                <MenuIcon onClick={this.handleMenu}/>
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <Link onClick={this.handleClose} to={routes.HOME}>Home</Link>
+                <Link onClick={this.handleClose} to={routes.ACCOUNT}>Account</Link>
+  
+              </Menu>
+            </div>
+            <Typography variant="title" color="inherit">
+              Staff Dashboard
+            </Typography>
+            <SignOutButton />
+          </Toolbar> 
+        </AppBar>
+  
+  
+      )
+  
+    }
+
+  
+  
+}
 
 export default Navigation;
--- src/components/SignOut/SignOut.js
+++ src/components/SignOut/SignOut.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 import { auth } from '../../firebase';
 
 const SignOutButton = () => {
   return (
-    <button
+    <Button
       type="button"
       onClick={auth.doSignOut}
+      style={{ 
+        display: "inline",
+        marginLeft: 'auto',
+        marginRight: '1vw',
+        marginTop: '1.5vh',
+        color: 'white'
+      }}
     >
       Sign Out
-    </button>
+    </Button>
   )
 }
 
